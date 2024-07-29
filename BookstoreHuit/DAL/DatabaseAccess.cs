e5be0db59c75c323de2927a6165faaa10457e894
Dinh Quang Cuong <127744213+dquangcuong@users.noreply.github.com>
2024-07-29 21:57:06 +0700

    completed

---
+++ BookstoreHuit/DAL/DatabaseAccess.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using DTO;
+
+namespace DAL
+{
+    public class SqlConnectionData
+    {
+        // Tạo chuỗi kết nối CSDL
+        public static SqlConnection Connect()
+        {
+            string connectionString = @"Data Source=QUANGCUONG\SQLEXPRESS;Initial Catalog=DA_QLSACH;User ID=sa;Password=123;Integrated Security=False;";
+            SqlConnection sqlconn = new SqlConnection(connectionString);
+            return sqlconn;
+        }
+    }
+
+    public class DatabaseAccess
+    {
+        public static string CheckDangNhapDTO(NhanVien nhanVien)
+        {
+            string user = null;
+
+            // Sử dụng using để đảm bảo SqlConnection được đóng và giải phóng tài nguyên
+            using (SqlConnection conn = SqlConnectionData.Connect())
+            {
+                conn.Open();
+
+                using (SqlCommand command = new SqlCommand("proc_dangnhap", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@user", nhanVien.UsernameNV);
+                    command.Parameters.AddWithValue("@pass", nhanVien.PasswordNV);
+
+                    // Sử dụng using để đảm bảo SqlDataReader được đóng và giải phóng tài nguyên
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                int u = reader.GetInt32(0);
+                                user = u.ToString();
+                                return user;
+                            }
+                        }
+                        else
+                        {
+                            return "Tài khoản hoặc mật khẩu không chính xác!";
+                        }
+                    }
+                }
+            }
+
+            return user;
+        }
+    }
+}
+
---
+++ BookstoreHuit/DA_STOREBOOKS_WIN/frmLogin.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace DA_STOREBOOKS_WIN
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.chk_HienMatKhau = new System.Windows.Forms.CheckBox();
+            this.txt_User = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txt_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Password.Location = new System.Drawing.Point(138, 262);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(260, 30);
+            this.txt_Password.TabIndex = 24;
+            this.txt_Password.UseSystemPasswordChar = true;
+            this.txt_Password.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txt_Password_KeyUp);
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.DarkGreen;
+            this.btn_Login.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Login.Location = new System.Drawing.Point(129, 346);
+            this.btn_Login.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(181, 48);
+            this.btn_Login.TabIndex = 23;
+            this.btn_Login.Text = "Đăng nhập";
+            this.btn_Login.UseVisualStyleBackColor = false;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // chk_HienMatKhau
+            // 
+            this.chk_HienMatKhau.AutoSize = true;
+            this.chk_HienMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.chk_HienMatKhau.Location = new System.Drawing.Point(145, 316);
+            this.chk_HienMatKhau.Margin = new System.Windows.Forms.Padding(4);
+            this.chk_HienMatKhau.Name = "chk_HienMatKhau";
+            this.chk_HienMatKhau.Size = new System.Drawing.Size(138, 22);
+            this.chk_HienMatKhau.TabIndex = 22;
+            this.chk_HienMatKhau.Text = "Hiện mật khẩu";
+            this.chk_HienMatKhau.UseVisualStyleBackColor = true;
+            this.chk_HienMatKhau.CheckedChanged += new System.EventHandler(this.chk_HienMatKhau_CheckedChanged);
+            // 
+            // txt_User
+            // 
+            this.txt_User.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txt_User.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_User.Location = new System.Drawing.Point(138, 199);
+            this.txt_User.Margin = new System.Windows.Forms.Padding(0);
+            this.txt_User.Name = "txt_User";
+            this.txt_User.Size = new System.Drawing.Size(260, 30);
+            this.txt_User.TabIndex = 21;
+            this.txt_User.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_User_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(158, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 17);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Tên Đăng Nhập:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(158, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Mật Khẩu:";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::DA_STOREBOOKS_WIN.Properties.Resources.anh_o_khoa;
+            this.pictureBox4.Image = global::DA_STOREBOOKS_WIN.Properties.Resources.anh_o_khoa;
+            this.pictureBox4.Location = new System.Drawing.Point(66, 251);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(55, 41);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 20;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::DA_STOREBOOKS_WIN.Properties.Resources.User;
+            this.pictureBox3.Image = global::DA_STOREBOOKS_WIN.Properties.Resources.User;
+            this.pictureBox3.Location = new System.Drawing.Point(66, 189);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(55, 40);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(927, 444);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(440, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(490, 443);
+            this.panel1.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(136, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 38);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "PHẦN MỀM";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(81, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(293, 38);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "QUẢN LÝ HIỆU SÁCH";
+            // 
+            // frmLogin
+            // 
+            this.AcceptButton = this.btn_Login;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 444);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.chk_HienMatKhau);
+            this.Controls.Add(this.txt_User);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Nhập";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.CheckBox chk_HienMatKhau;
+        private System.Windows.Forms.TextBox txt_User;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
